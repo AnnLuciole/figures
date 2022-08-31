9cb7a8740ecf38963e35baa4c240d832a58afd93
Anna Grigorenko <annluciole@mail.ru>
2022-08-31 14:14:50 +0500

    Added packages Quadrangle,Triangles and class Figures.

---
+++ src/Figures/Figures.java
@@ -0,0 +1,13 @@
+package Figures;
+
+public class Figures {
+    private int numOfAngle;
+
+    public int getNumOfAngle() {
+        return numOfAngle;
+    }
+
+    public void setNumOfAngle(int numOfAngle) {
+        this.numOfAngle = numOfAngle;
+    }
+}
---
+++ src/Figures/Triangles/IsoscelesTriangle.java
@@ -0,0 +1,10 @@
+package Figures.Triangles;
+
+import Figures.Figures;
+
+public class IsoscelesTriangle extends Figures {
+
+    IsoscelesTriangle() {
+        setNumOfAngle(3);
+    }
+}
