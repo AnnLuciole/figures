d2383319e492d97a8a14af27a6291437a9cc6d73
Anna Grigorenko <annluciole@mail.ru>
2022-09-01 13:44:53 +0500

    Added Main.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import Figures.Circle;
+import Figures.Figures;
+
+public class Main {
+    public static void main(String[] args) {
+        Figures circleOne = new Circle(3);
+        System.out.println(circleOne.getArea());
+    }
+}
